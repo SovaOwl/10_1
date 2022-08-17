7a21c3334006e5428fc35b55729c95043a51819b
SovaOwl <sova27755@gmail.com>
2022-08-17 22:02:44 +0300

    com

--- Source1.cpp
+++ Source1.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main()
 {
 	system("chcp 1251");
-	setlocale(LC_ALL, "Rus");
 
 	string name = "<3";
 
-	cout << "Введите имя: ";
+	cout << "Enter your name: ";
 	cin >> name;
-	cout << "Здравствуйте, " << name << "!" << endl;
+	cout << "Hello, " << name << "!" << endl;
 }
